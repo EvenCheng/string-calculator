e1dca03c91b2f1ca3c4414e1f3beddbb561ac609
Even Cheng <even.gamer@gmail.com>
2024-09-06 23:49:02 -0700

    req 4 - handle neg numbers

--- Calculator.cs
+++ Calculator.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace StringCalculator
 {
@@ -14,18 +16,35 @@ namespace StringCalculator
 
             var numbers = input.Split(',');
 
+            // List to store any negative numbers found
+            List<int> negativeNumbers = new List<int>();
+
             int sum = 0;
 
             foreach (var number in numbers)
             {
                 if (int.TryParse(number, out int result))
                 {
-                    sum += result;
+                    if (result < 0)
+                    {
+                        // Add negative number to the list
+                        negativeNumbers.Add(result);
+                    }
+                    else
+                    {
+                        sum += result;
+                    }
                 }
                 else
                 {
                     sum += 0; // Invalid numbers are treated as 0
                 }
+            }
+
+            // If there are negative numbers, throw an exception
+            if (negativeNumbers.Any())
+            {
+                throw new ArgumentException($"Negatives not allowed: {string.Join(", ", negativeNumbers)}");
             }
 
             return sum;
--- StringCalulator.Tests/CalculatorTests.cs
+++ StringCalulator.Tests/CalculatorTests.cs
@@ -16,131 +16,93 @@ namespace StringCalculator.Tests
         [Fact]
         public void Add_EmptyString_ReturnsZero()
         {
-            // Arrange
             var input = "";
-
-            // Act
             var result = _calculator.Add(input);
-
-            // Assert
             Assert.Equal(0, result);
         }
 
         [Fact]
         public void Add_SingleNumber_ReturnsThatNumber()
         {
-            // Arrange
             var input = "20";
-
-            // Act
             var result = _calculator.Add(input);
-
-            // Assert
             Assert.Equal(20, result);
         }
 
         [Fact]
         public void Add_TwoNumbers_ReturnsTheirSum()
         {
-            // Arrange
             var input = "1,5000";
-
-            // Act
             var result = _calculator.Add(input);
-
-            // Assert
             Assert.Equal(5001, result);
         }
 
         [Fact]
-        public void Add_NegativeNumber_ReturnsSum()
+        public void Add_NegativeNumber_ThrowsException()
         {
-            // Arrange
             var input = "4,-3";
+            
+            var exception = Assert.Throws<ArgumentException>(() => _calculator.Add(input));
 
-            // Act
-            var result = _calculator.Add(input);
+            Assert.Equal("Negatives not allowed: -3", exception.Message);
+        }
 
-            // Assert
-            Assert.Equal(1, result);
+        [Fact]
+        public void Add_MultipleNegativeNumbers_ThrowsExceptionWithAllNegatives()
+        {
+            var input = "1,-2,3,-4,5";
+            
+            var exception = Assert.Throws<ArgumentException>(() => _calculator.Add(input));
+
+            Assert.Equal("Negatives not allowed: -2, -4", exception.Message);
         }
 
         [Fact]
         public void Add_MultipleNumbers_ReturnsTheirSum()
         {
-            // Arrange
             var input = "1,2,3,4,5,6,7,8,9,10,11,12";
-
-            // Act
             var result = _calculator.Add(input);
-
-            // Assert
             Assert.Equal(78, result);
         }
 
         [Fact]
         public void Add_InvalidNumber_TreatsAsZero()
         {
-            // Arrange
             var input = "5,tytyt";
-
-            // Act
             var result = _calculator.Add(input);
-
-            // Assert
             Assert.Equal(5, result);
-        }
-
-        [Fact]
-        public void Add_OneValidAndOneEmptyNumber_ReturnsValidNumber()
-        {
-            // Arrange
-            var input = "4,";
-
-            // Act
-            var result = _calculator.Add(input);
-
-            // Assert
-            Assert.Equal(4, result);
-        }
-
-        [Fact]
-        public void Add_AllInvalidNumbers_ReturnsZero()
-        {
-            // Arrange
-            var input = "abc,xyz";
-
-            // Act
-            var result = _calculator.Add(input);
-
-            // Assert
-            Assert.Equal(0, result);
         }
 
         [Fact]
         public void Add_NewlineAsDelimiter_ReturnsSum()
         {
-            // Arrange
             var input = "1\n2,3";
-
-            // Act
             var result = _calculator.Add(input);
-
-            // Assert
             Assert.Equal(6, result);
         }
 
         [Fact]
         public void Add_MultipleNewlineAndCommaDelimiters_ReturnsSum()
         {
-            // Arrange
             var input = "1\n2\n3\n4,\n5";
-
-            // Act
             var result = _calculator.Add(input);
-
-            // Assert
             Assert.Equal(15, result);
+        }
+
+        [Fact]
+        public void Add_OneValidAndOneEmptyNumber_ReturnsValidNumber()
+        {
+            var input = "4,";
+            var result = _calculator.Add(input);
+            Assert.Equal(4, result);
+        }
+
+        [Fact]
+        public void Add_AllInvalidNumbers_ReturnsZero()
+        {
+            var input = "abc,xyz";
+            var result = _calculator.Add(input);
+            Assert.Equal(0, result);
         }
     }
 }
