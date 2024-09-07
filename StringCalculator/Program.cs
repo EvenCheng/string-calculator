091be9d923bcd6eaef5cbdbd2d92984d4c2839cb
Even Cheng <even.gamer@gmail.com>
2024-09-07 15:24:06 -0700

    stretch #2 - Allow the application to process entered entries until Ctrl+C is used.

---
+++ StringCalculator/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using StringCalculator;
+
+namespace StringCalculatorApp
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            var calculator = new Calculator();
+            Console.WriteLine("Enter numbers to add. Press Ctrl+C to exit.");
+
+            while (true)
+            {
+                Console.Write("Input: ");
+                string input = Console.ReadLine();
+                try
+                {
+                    var result = calculator.Add(input);
+                    Console.WriteLine(result.ToString());
+                }
+                catch (ArgumentException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+    }
+}
